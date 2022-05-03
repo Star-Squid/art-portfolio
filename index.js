3773c486e0b58525d90d84bf8d670958e661f35f
Star-Squid <81830542+Star-Squid@users.noreply.github.com>
2022-05-03 14:29:10 +0100

    Key navigation in modals +
    Thumbnails on mobile max 40vw, arrows on mobile a little smaller, align text left on mobile, stop scrolling under modal, try another font that goes with raleway for titles, add keyboard and swipe actions, correct the console error

--- index.js
+++ index.js
@@ -1,27 +1,101 @@
 // CONSOLE MESSAGE
-console.log("%c   __,                __,                   __,\n (    _/_           (            o    /   (    _/_       / o\n  `.  /  __,  _      `.  _,  , ,,  __/     `.  /  , , __/ , _\n(___)(__(_/(_/ (_  (___)(_/_(_/_(_(_/_   (___)(__(_/_(_/_(_(_)\n                         /|\n                         (/\ncreated by Barbara Malta :: thanks for peeking under the hood! :: hello@star-squid.art", "font-family:monospace; color:green")
+console.log(
+  "%c   __,                __,                   __,\n (    _/_           (            o    /   (    _/_       / o\n  `.  /  __,  _      `.  _,  , ,,  __/     `.  /  , , __/ , _\n(___)(__(_/(_/ (_  (___)(_/_(_/_(_(_/_   (___)(__(_/_(_/_(_(_)\n                         /|\n                         (/\ncreated by Barbara Malta :: thanks for peeking under the hood! :: hello@star-squid.art",
+  "font-family:monospace; color:green"
+);
+
+// EVERY SECTION - arrow key and escape actions in modals
+document.addEventListener("keydown", function (event) {
+  var key = event.key;
+  //console.log(key);
+  switch (key) {
+    case "ArrowLeft":
+      changeCreatSlides(-1);
+      changeFaceSlides(-1);
+      changePlantSlides(-1);
+      changePlaceSlides(-1);
+      break;
+    case "ArrowRight":
+      changeCreatSlides(1);
+      changeFaceSlides(1);
+      changePlantSlides(1);
+      changePlaceSlides(1);
+      break;
+    case "Escape":
+      // closeModal('creat');
+      // closeModal('face');
+      // closeModal('place');
+      // closeModal('plant');
+      closeModal(currentSection);
+      break;
+  }
+});
+
+// PREVENT MAIN BODY FOCUS WHEN IN A MODAL
+//find visible elements that can be tabbed to - only run after a collapsible is open, to include thumbnails
+function tabbable(el) {
+  return [].map
+    .call(
+      el.querySelectorAll([
+        "input",
+        "select",
+        "a[href]",
+        "textarea",
+        "button",
+        "[tabindex]",
+      ]),
+      function (el, i) {
+        return { el, i };
+      }
+    )
+    .filter(function (e) {
+      return e.el.tabIndex >= 0 && !e.el.disabled && e.el.offsetParent;
+    })
+    .sort(function (a, b) {
+      return a.el.tabIndex === b.el.tabIndex
+        ? a.i - b.i
+        : (a.el.tabIndex || 9e9) - (b.el.tabIndex || 9e9);
+    });
+}
+
+let tabbableElements;
+
+//remove all from tab order
+function makeUntabbable(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].el.tabIndex = -1;
+  }
+}
+
+//enable tabbing again
+function makeTabbable(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].el.tabIndex = 0;
+  }
+}
 
 // COLLAPSE MENU ITEMS
-
 //select all collapsible elements
 var collapsibles = document.getElementsByClassName("collapsible");
 
 //when a section title gets clicked, make visible the sibling of the title, i.e. content
 for (var i = 0; i < collapsibles.length; i++) {
-  collapsibles[i].addEventListener("click", function() {
+  collapsibles[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "flex") {
       content.style.display = "none";
     } else {
       content.style.display = "flex";
+      tabbableElements = tabbable(document);
+      //console.log(tabbableElements);
     }
   });
 }
 
 // pullout animation
 for (var i = 0; i < collapsibles.length; i++) {
-  collapsibles[i].addEventListener("click", function() {
+  collapsibles[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.maxHeight) {
@@ -37,19 +111,38 @@ for (var i = 0; i < collapsibles.length; i++) {
 var currentSection;
 
 function openModal(section) {
+  makeUntabbable(tabbableElements);
+  //console.log(tabbableElements);
+
+  const body = document.body;
+  body.classList.add("do-not-scroll");
+  // const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+  // const body = document.body;
+  // body.style.height = '100vh';
+  // body.style.overflowY = 'hidden';
+  // body.style.position = 'fixed';
+
   var modalName = "modal-" + section;
   document.getElementById(modalName).style.display = "block";
   currentSection = section;
-  console.log(currentSection);
-
+  //console.log(currentSection);
 }
 
 // Close all Modals
 function closeModal() {
+  makeTabbable(tabbableElements);
+
+  const body = document.body;
+  body.classList.remove("do-not-scroll");
+  // const body = document.body;
+  // body.style.position = '';
+  // body.style.top = '';
+  // body.style.height = '';
+  // body.style.overflowY = '';
+
   var modalName = "modal-" + currentSection;
   document.getElementById(modalName).style.display = "none";
 }
-
 
 // CREATURES
 
@@ -60,11 +153,11 @@ function showCreatSlides(n) {
   var slideName = "slide-" + currentSection;
   var slides = document.getElementsByClassName(slideName);
   if (n > slides.length) {
-    slideIndexCreat = 1
-  };
+    slideIndexCreat = 1;
+  }
   if (n < 1) {
-    slideIndexCreat = slides.length
-  };
+    slideIndexCreat = slides.length;
+  }
   for (var i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -73,18 +166,17 @@ function showCreatSlides(n) {
 
 //show slides starting from x
 var slideIndexCreat = 1;
-showCreatSlides(slideIndexCreat);
+//showCreatSlides(slideIndexCreat);
 
 //increase/decrease which slide is shown
 function changeCreatSlides(n) {
-  showCreatSlides(slideIndexCreat += n);
+  showCreatSlides((slideIndexCreat += n));
 }
 
 //set modal to selected image
 function currentCreatSlide(n) {
-  showCreatSlides(slideIndexCreat = n);
+  showCreatSlides((slideIndexCreat = n));
 }
-
 
 //TRYING TO WRITE A SINGLE FUNCTION to replace show"X"Slides() in each section
 //to be used in current"X"Slide and change"X"Slides
@@ -102,7 +194,6 @@ function currentCreatSlide(n) {
 //   slides[slideIndexCreat-1].style.display = "block";
 // }
 
-
 // FACES
 
 var slideIndexFace = 1;
@@ -110,29 +201,28 @@ showFaceSlides(slideIndexFace);
 
 // Next/previous controls
 function changeFaceSlides(n) {
-  showFaceSlides(slideIndexFace += n);
+  showFaceSlides((slideIndexFace += n));
 }
 
 // Thumbnail image controls
 function currentFaceSlide(n) {
-  showFaceSlides(slideIndexFace = n);
+  showFaceSlides((slideIndexFace = n));
 }
 
 function showFaceSlides(n) {
   var i;
   var slidesFace = document.getElementsByClassName("slide-face");
   if (n > slidesFace.length) {
-    slideIndexFace = 1
+    slideIndexFace = 1;
   }
   if (n < 1) {
-    slideIndexFace = slidesFace.length
+    slideIndexFace = slidesFace.length;
   }
   for (i = 0; i < slidesFace.length; i++) {
     slidesFace[i].style.display = "none";
   }
   slidesFace[slideIndexFace - 1].style.display = "block";
 }
-
 
 ////// PLACES
 
@@ -141,29 +231,28 @@ showPlaceSlides(slideIndexPlace);
 
 // Next/previous controls
 function changePlaceSlides(n) {
-  showPlaceSlides(slideIndexPlace += n);
+  showPlaceSlides((slideIndexPlace += n));
 }
 
 // Thumbnail image controls
 function currentPlaceSlide(n) {
-  showPlaceSlides(slideIndexPlace = n);
+  showPlaceSlides((slideIndexPlace = n));
 }
 
 function showPlaceSlides(n) {
   var i;
   var slidesPlace = document.getElementsByClassName("slide-place");
   if (n > slidesPlace.length) {
-    slideIndexPlace = 1
+    slideIndexPlace = 1;
   }
   if (n < 1) {
-    slideIndexPlace = slidesPlace.length
+    slideIndexPlace = slidesPlace.length;
   }
   for (i = 0; i < slidesPlace.length; i++) {
     slidesPlace[i].style.display = "none";
   }
   slidesPlace[slideIndexPlace - 1].style.display = "block";
 }
-
 
 //PLANTS
 
@@ -172,54 +261,25 @@ showPlantSlides(slideIndexPlant);
 
 // Next/previous controls
 function changePlantSlides(n) {
-  showPlantSlides(slideIndexPlant += n);
+  showPlantSlides((slideIndexPlant += n));
 }
 
 // Thumbnail image controls
 function currentPlantSlide(n) {
-  showPlantSlides(slideIndexPlant = n);
+  showPlantSlides((slideIndexPlant = n));
 }
 
 function showPlantSlides(n) {
   var i;
   var slidesPlant = document.getElementsByClassName("slide-plant");
   if (n > slidesPlant.length) {
-    slideIndexPlant = 1
+    slideIndexPlant = 1;
   }
   if (n < 1) {
-    slideIndexPlant = slidesPlant.length
+    slideIndexPlant = slidesPlant.length;
   }
   for (i = 0; i < slidesPlant.length; i++) {
     slidesPlant[i].style.display = "none";
   }
   slidesPlant[slideIndexPlant - 1].style.display = "block";
 }
-
-
-// EVERY SECTION - arrow key and escape actions
-// needs work
-document.addEventListener('keydown', function(event) {
-  var key = event.key;
-  console.log(key);
-  switch (key) {
-    case "ArrowLeft":
-      changeCreatSlides(-1);
-      changeFaceSlides(-1);
-      changePlantSlides(-1);
-      changePlaceSlides(-1);
-      break;
-    case "ArrowRight":
-      changeCreatSlides(1);
-      changeFaceSlides(1);
-      changePlantSlides(1);
-      changePlaceSlides(1);
-      break;
-    case "Escape":
-      // closeModal('creat')
-      // closeModal('face')
-      // closeModal('place')
-      // closeModal('plant')
-      closeModal(currentSection);
-      break;
-  }
-});
